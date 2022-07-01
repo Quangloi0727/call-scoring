c8fed3d61ee14876da2dc7a6bffce626f29e273e
loinq <loinq@metechvn.com>
2022-07-01 22:26:09 +0700

    add var1 to var 10

---
+++ src/constants/constants.js
@@ -0,0 +1,27 @@
+// cấu hình bảng mặc định
+const headerDefault = {
+    callId: "ID cuộc gọi",
+    direction: "Hướng gọi",
+    agentName: "Điện thoại viên",
+    teamName: "Đội ngũ",
+    caller: "Số gọi đi",
+    called: "Số gọi đến",
+    origTime: "Ngày giờ gọi",
+    duration: "Thời lượng",
+    audioHtml: "Ghi âm",
+    sourceName: "Nguồn ghi âm",
+    var1: "Var1",
+    var2: "Var2",
+    var3: "Var3",
+    var4: "Var4",
+    var5: "Var5",
+    var6: "Var6",
+    var7: "Var7",
+    var8: "Var8",
+    var9: "Var9",
+    var10: "Var10"
+}
+
+module.exports = {
+    headerDefault
+}
