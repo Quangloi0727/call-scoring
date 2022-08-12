42bdd808af89c3352ec4bfa19e4a7214fc3d8a79
nguyenvc <vunguyen140220@gamil.com>
2022-08-12 11:04:21 +0700

    ds mục tiêu chấm điểm

--- dev.build.sh
+++ dev.build.sh
@@ -2,6 +2,7 @@
 version=$1
 
 docker build -t mt-recording:$version .
+#docker buildx build --platform=linux/amd64 -t mt-recording:$version . chạy trên máy mac
 
 #Push image to docker registry
 docker tag mt-recording:$version 172.16.16.110:5000/mt-recording:$version
