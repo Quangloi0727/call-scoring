a5e997bd24ae62c369fe676ae6925811df9968fa
nguyenvc <vunguyen140220@gamil.com>
2022-08-16 22:20:40 +0700

    fix

---
+++ src/models/scoreTarget_scoreScript.js
@@ -0,0 +1,42 @@
+const { Model, DataTypes, Op } = require("sequelize")
+
+class ScoreTargets_ScoreScripts extends Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        scoreScriptId: {
+          type: DataTypes.INTEGER,
+          references: {
+            model: "ScoreScripts",
+            key: "id",
+          },
+        },
+        scoreTargetId: {
+          type: DataTypes.INTEGER,
+          references: {
+            model: "ScoreTargets",
+            key: "id",
+          }
+        }
+      },
+      {
+        sequelize,
+        modelName: "ScoreTargets_ScoreScripts",
+      }
+    )
+  }
+
+  static associate(models) {
+    models.ScoreTarget_ScoreScript.belongsTo(models.ScoreScript, {
+      foreignKey: "scoreScriptId",
+      as: "ScoreScripts",
+    })
+
+    models.ScoreTarget_ScoreScript.belongsTo(models.ScoreTarget, {
+      foreignKey: "scoreTargetId",
+      as: "ScoreTargets",
+    })
+  }
+}
+
+module.exports = ScoreTargets_ScoreScripts
