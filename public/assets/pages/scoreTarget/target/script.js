6d73431513ad87be332c6e1439abf7f850f8b532
loinq <loinq@metechvn.com>
2022-08-17 16:15:54 +0700

    update

--- public/assets/pages/scoreTarget/target/script.js
+++ public/assets/pages/scoreTarget/target/script.js
@@ -518,6 +518,7 @@ function disableOrEnableButton() {
   $("#effectiveTime").prop("disabled", true)
   $('select').prop('disabled', true)
   $("#scoreScriptId").prop("disabled", false)
+  $("#status").attr("disabled", false)
 
   $("#btn-add-conditions").remove()
   $(".btn-add-row-target-auto").html('')
